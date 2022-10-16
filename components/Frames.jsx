df80a725f0975053716a69ab70b127b1e8f88ef2
Khan <namia.khan@dxc.com>
2022-10-16 08:27:10 +0530

    added cards using CSS

--- components/Frames.jsx
+++ components/Frames.jsx
@@ -3,23 +3,23 @@ import { Data } from "../data/contextProvider";
 
 const CardMapping = () => {
   return (
-    <div className="flex flex-col items-center justify-center md:grid md:grid-cols-2 md:gap-5 lg:grid lg:grid-cols-3 lg:gap-5 2xl:grid 2xl:grid-cols-5 2xl:gap-10 justify-items-center ">
+    <div className="flex flex-col items-center justify-center md:grid md:grid-cols-2 md:gap-5 xl:grid xl:grid-cols-4 xl:gap-5 2xl:grid 2xl:grid-cols-5 2xl:gap-5 justify-items-center space-y-10 md:space-y-0 ">
       {Data.map((coins) => (
-        <div key={coins.id} className="mt-10 md:mt-5 flex flex-col items-center ">
-          <Image
-            src={coins.url}
-            alt="Picture of the author"
-            width={100}
-            height={100}
-          />
-          <div className="cards w-[290px] h-[350px] -mt-12">
-            {/* Coin*/}
-            <div className="pt-20 p-5 flex flex-col items-center justify-center">
+        <div key={coins.id} className="card">
+          <div className="icon">
+            <Image
+              src={coins.url}
+              alt="Picture of the author"
+              width={90}
+              height={90}
+            />
+          </div>
+          <div className="content">
+            <div className=" p-5 flex flex-col items-center justify-center">
               <h4>{coins.coin}</h4>
               <div className="rectangle mt-3 w-full h-[40px]">
                 <div className="flex flex-row items-center justify-between px-14 p-2">
                   <h2>{coins.price1}</h2>
-                  {/* <h3>{coins.rate}</h3> */}
                   {coins.rate < 0 ? (
                     <h3 className="red">{coins.rate}%</h3>
                   ) : (
@@ -27,14 +27,12 @@ const CardMapping = () => {
                   )}
                 </div>
               </div>
-              {/* Price */}
               <h5>Price</h5>
               <div className="rectangle  w-full h-[40px]">
                 <div className="flex flex-row items-center justify-center px-10 p-2">
                   <h2>$60,000</h2>
                 </div>
               </div>
-              {/* TVL */}
               <h5>TVL</h5>
               <div className="rectangle  w-full h-[40px]">
                 <div className="flex flex-row items-center  justify-evenly px-10 p-2">
@@ -71,10 +69,8 @@ const Home = () => {
   return (
     <div className="w-screen h-full bg-darkBlue px-10">
       <div className="flex flex-row justify-center  items-center ">
-        <div className="flex flex-col h-full lg:h-screen w-screen items-center 2xl:items-start justify-center">
-          {/* Heading */}
-
-          <div className="flex flex-row items-center justify-center space-x-2 mt-10 xl:mt-0">
+        <div className="flex flex-col h-full xl:h-screen w-screen items-center 2xl:items-start justify-center ">
+          <div className="flex flex-row items-center justify-center space-x-2  mb-10 mt-10 2xl:mt-0 ">
             <Image
               src="/images/activity.png"
               alt="Picture of the author"
@@ -83,7 +79,6 @@ const Home = () => {
             />
             <h1>Trending Assets</h1>
           </div>
-
           <CardMapping />
         </div>
       </div>
